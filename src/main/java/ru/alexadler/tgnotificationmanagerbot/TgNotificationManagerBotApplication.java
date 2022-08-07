febf63259fdbcf66083fc4f6a28b34bea9e4faa3
Alexandra <alexadler@bk.ru>
2022-08-07 16:04:07 +0500

    Initial commit

---
+++ src/main/java/ru/alexadler/tgnotificationmanagerbot/TgNotificationManagerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.alexadler.tgnotificationmanagerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgNotificationManagerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgNotificationManagerBotApplication.class, args);
+	}
+
+}
