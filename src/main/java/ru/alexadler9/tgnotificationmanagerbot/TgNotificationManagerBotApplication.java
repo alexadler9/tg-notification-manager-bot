8d684f18f8b290dbb6aae6e6873f55fae492fb2f
alexadler9 <alexadler@bk.ru>
2023-12-02 17:14:29 +0500

    Initial commit

---
+++ src/main/java/ru/alexadler9/tgnotificationmanagerbot/TgNotificationManagerBotApplication.java
@@ -0,0 +1,12 @@
+package ru.alexadler9.tgnotificationmanagerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgNotificationManagerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgNotificationManagerBotApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/alexadler9/tgnotificationmanagerbot/TgNotificationManagerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexadler9.tgnotificationmanagerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgNotificationManagerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
